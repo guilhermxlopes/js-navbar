4f8ad621924d69c8554470f41895cb7c86480de1
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-05-20 16:40:38 -0300

    JS mobile navbar

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const navSlide = () => {
   const burguer = document.querySelector(".burguer");
   const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
+  //toggle nav
   burguer.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.8
+        }s`;
+      }
+    });
   });
+
+  //animate links
 };
 
 navSlide();
